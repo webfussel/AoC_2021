a06f5aa1318e0e9453f2f8b561b9e59d3e0f31fe
Fiona Toma <fiona.lena.toma@gmail.com>
2021-12-02 08:04:31 +0100

    refactor day 2

--- day_02/app.js
+++ day_02/app.js
@@ -2,36 +2,24 @@ const fs = require('fs')
 const nav = fs.readFileSync('./navigation.txt').toString().split('\n')
 
 // Part 1
-const coords = {
-    pos: 0,
-    depth: 0
-}
-
-nav.forEach(e => {
-    const [,direction, amount] = e.match(/(.*?) (\d)/)
-    switch(direction) {
-        case 'forward': coords.pos += +amount; break;
-        case 'down': coords.depth += +amount; break;
-        case 'up': coords.depth -= +amount; break;
+const coords = nav.reduce((r, c) => {
+    const [,dir, amount] = c.match(/(.*?) (\d)/)
+    return {
+        pos: dir === 'forward' ? r.pos + +amount : r.pos,
+        depth: dir === 'down' ? r.depth + +amount : dir !== 'forward' ? r.depth - +amount : r.depth
     }
-})
+}, {pos: 0, depth: 0})
 
 console.log(`Pos: ${coords.pos} | Depth: ${coords.depth} | Final: ${coords.pos * coords.depth}`)
 
 // Part 2
-const course = {
-    pos: 0,
-    depth: 0,
-    aim: 0
-}
-
-nav.forEach(e => {
-    const [,direction, amount] = e.match(/(.*?) (\d)/)
-    switch(direction) {
-        case 'forward': course.pos += +amount; course.depth += amount * course.aim; break;
-        case 'down': course.aim += +amount; break;
-        case 'up': course.aim -= +amount; break;
+const course = nav.reduce((r, c) => {
+    const [,dir, amount] = c.match(/(.*?) (\d)/)
+    return {
+        pos: dir === 'forward' ? r.pos + +amount : r.pos,
+        aim: dir === 'down' ? r.aim + +amount : dir !== 'forward' ? r.aim - +amount : r.aim,
+        depth: dir === 'forward' ? r.depth + amount * r.aim : r.depth
     }
-})
+}, {pos: 0, aim: 0, depth: 0})
 
 console.log(`Pos: ${course.pos} | Depth: ${course.depth} | Aim: ${course.aim} | Final: ${course.pos * course.depth}`)
