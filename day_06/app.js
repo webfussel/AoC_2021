992603460c605390dce04f4f869f724f4c6e9840
Fiona Toma <fiona.lena.toma@gmail.com>
2021-12-06 15:36:04 +0100

    CHANGE Day 6 - some code adjustments

--- day_06/app.js
+++ day_06/app.js
@@ -1,24 +1,17 @@
 const fs = require('fs')
-const fish = fs.readFileSync('./fish.txt').toString().trim().split(',').map(e => +e)
+const fish = fs.readFileSync('./fish.txt').toString().trim().split(',').map(e => +e).reduce((f, c) => ({...f, [c]: BigInt(~~f[c]) + BigInt(1)}), {})
 
 // General
 const newBorn = 8
 const resetTo = 6
-const initialFishies = fish.reduce((f, c) => ({...f, [c]: BigInt(~~f[c]) + BigInt(1)}), {})
 
 const increaseFishly = (fishies) => {
     const newBorns = [newBorn, BigInt(0)]
     return [...Object.entries(fishies).map(([days, amount],i,arr) => {
-        let newDay = days - 1
-        if (newDay < 0) {
-            newDay = resetTo
-            newBorns[1] = BigInt(amount)
-        }
-        if (+days === 0 || +days === resetTo+1) {
-            return [newDay, BigInt(~~fishies[0]) + BigInt(~~fishies[resetTo+1])]
-        } else {
-            return [newDay, BigInt(amount)]
-        }
+        let newDay = days-1 < 0 ? resetTo : days-1
+        days-1 < 0 && (newBorns[1] = BigInt(amount))
+        const newAmount = +days === 0 || +days === resetTo+1 ? BigInt(~~fishies[0]) + BigInt(~~fishies[resetTo+1]) : BigInt(amount)
+        return [newDay, newAmount]
     }),newBorns].reduce((f,c) => ({...f, [c[0]]: BigInt(c[1])}),{})
 }
 
@@ -31,7 +24,7 @@ const calcFishies = (fish, days) => {
 }
 
 // Part 1
-console.log(`Part 1: ${calcFishies(initialFishies, 80)}`)
+console.log(`Part 1: ${calcFishies(fish, 80)}`)
 
 // Part 2
-console.log(`Part 2: ${calcFishies(initialFishies, 256)}`)
+console.log(`Part 2: ${calcFishies(fish, 256)}`)
